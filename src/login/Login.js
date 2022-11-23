92e0ea88aa0206d2e7212947dafd911767f967f3
Felix Weiß <felixweiss@gmx.at>
2022-11-23 20:25:08 +0000

    added Login and Register

--- src/login/Login.js
+++ src/login/Login.js
@@ -3,7 +3,17 @@ import "./Login.css";
 const Login = () => {
     return ( 
         <div className="wrapperLogin flex">
-            
+            <div className="login flex">
+                <label>
+                    <b>Username</b>
+                </label>
+                <input type="text" placeholder="Enter Username" name="username" required></input>
+                <label>
+                    <b>Password</b>
+                </label>
+                <input type="text" placeholder="Enter Password" name="password" required></input>
+                <button className="loginbutton" type="submit">Login</button>
+            </div>
         </div>
     );
 }
